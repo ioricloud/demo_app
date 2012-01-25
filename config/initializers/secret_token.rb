22ccd8c40b82db139b6050e8a944654a57129c00
henrique oliveira <ioricloud@gmail.com>
2012-01-24 23:52:13 -0300

    inicial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8ff01f7a4bdd374706becd3d729d6b8c118c3dd9559504a772e2148eb4290fa85bbc30444bd511b8c928268ffcbed7fda87e4d98a91b97ee6cc09959b352fc59'
